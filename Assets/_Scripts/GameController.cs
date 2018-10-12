91232ea9bf46d4dd5786409108e11d565dd612d8
TngBlt <tanguy21b@hotmail.fr>
2018-10-12 16:28:43 +0200

    Random position and gaze with no scale for now

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -2,21 +2,90 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
-	public GridController gridController;
+    private float CENTER_X_L;
+    private float CENTER_Y_C;
+    private float CENTER_X_R;
+    private float CENTER_Y_T;
+    public GridController gridController;
+    public GameObject wall;
+    public List<TargetCube> targets;
+    private float wall_width = 1;
+    private float wall_height = 1;
+    private static System.Random rand = new System.Random();
+    private int last_index;
+    private int target_index;
+    private float timeLeft = 1.0f;
+    private TargetCube last_target;
+    private RaycastHit current_target;
 
-	private RaycastHit current_target;
+    void Start()
+    {
+        //target_script = GetComponent<TargetCube>();
 
-	void Start() {
-		// gridController = gameObject.GetComponent<GridController>();
-	}
+        // Create const value for x, y min and max
+        CENTER_X_L = wall_width / 3;
+        CENTER_Y_C = wall_height / 3;
+        CENTER_X_R = wall_width - wall_width / 3;
+        CENTER_Y_T = wall_height - wall_height / 3;
 
-	// Update is called once per frame
-	void Update () {
-		current_target = gridController.GetCurrentCollider();
-		print(current_target.collider.tag);
+        // Create the targets
+        targets = new List<TargetCube>();
+        targets.Add(new TargetCube(0, CENTER_X_L, 0, CENTER_Y_C));
+        targets.Add(new TargetCube(CENTER_X_L, CENTER_X_R, 0, CENTER_Y_C));
+        targets.Add(new TargetCube(CENTER_X_R, wall_width, 0, CENTER_Y_C));
+        targets.Add(new TargetCube(0, CENTER_X_L, CENTER_Y_C, CENTER_Y_T));
+        targets.Add(new TargetCube(CENTER_X_L, CENTER_X_R, CENTER_Y_C, CENTER_Y_T));
+        targets.Add(new TargetCube(CENTER_X_R, wall_width, CENTER_Y_C, CENTER_Y_T));
+        targets.Add(new TargetCube(0, CENTER_X_L, CENTER_Y_T, wall_height));
+        targets.Add(new TargetCube(CENTER_X_L, CENTER_X_R, CENTER_Y_T, wall_height));
+        targets.Add(new TargetCube(CENTER_X_R, wall_width, CENTER_Y_T, wall_height));
 
-		// YO
-	}
+        // gridController = gameObject.GetComponent<GridController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the current object looked at
+        current_target = gridController.GetCurrentCollider();
+        //print(current_target.collider.tag);
+
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            // Reset timer 
+            timeLeft = 1.0f;
+            // Destroy last target
+            if (last_target != null)
+            {
+                last_target.DestroyTarget();
+            }
+            // Get a random target and spawn it
+            TargetCube trgt = selectItem();
+             if (current_target.collider.name == "Cube")
+            {
+                last_target.was_looked = true;
+            }
+            trgt.CreateTarget(wall);
+            last_target = trgt;
+
+
+        }
+    }
+
+    public TargetCube selectItem()
+    {
+        // Get a random target in the list
+        do
+        {
+            target_index = rand.Next(targets.Count);
+        } while (last_index == target_index);
+        last_index = target_index;
+        TargetCube target = targets[target_index];
+        return target;
+    }
+
 }
---
+++ Assets/_Scripts/TargetCube.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetCube
+{
+    private Vector3 previous_scale;
+    public float default_x_max;
+    private float default_y_max;
+    private float default_x_min;
+    private float default_y_min;
+    public float x_min;
+    public float y_min;
+    public float x_max;
+    public float y_max;
+    private GameObject cube;
+    public bool was_looked;
+    public TargetCube(float x_min, float x_max, float y_min, float y_max)
+    {
+        default_x_min = x_min;
+        default_x_max = x_max;
+        default_y_min = y_min;
+        default_y_max = y_max;
+        previous_scale = new Vector3(0.2f, 0.2f, 0.1f);
+    }
+    public void CreateTarget(GameObject wall)
+    {
+        // Create the Cube 
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        // Set the cube as child of the wall
+        cube.transform.parent = wall.transform;
+        if (was_looked)
+        {
+            // Ne passe pas par ici, TODO a regarder.
+           cube.transform.localScale -= new Vector3(0.05f,0.05f,0);
+        }
+        else
+        {
+            cube.transform.localScale = previous_scale;
+        }
+        CalculateOffset();
+        cube.transform.localPosition = new Vector3(Random.Range(x_min, x_max), Random.Range(y_min, y_max), -0.5f);
+        previous_scale = cube.transform.localScale;
+        was_looked = false;
+    }
+    private void CalculateOffset()
+    {
+        x_min = default_x_min + cube.transform.localScale.x / 2;
+        x_max = default_x_max - cube.transform.localScale.x / 2;
+        y_min = default_y_min + cube.transform.localScale.y / 2;
+        y_max = default_y_max - cube.transform.localScale.y / 2;
+    }
+    public void DestroyTarget()
+    {
+        Object.Destroy(cube);
+    }
+}
