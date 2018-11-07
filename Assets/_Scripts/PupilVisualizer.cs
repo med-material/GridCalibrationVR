1c8d25a49f8fac100df6947681416d86904570af
Shiks67 <ahmed.sdlv@gmail.com>
2018-11-07 11:14:06 +0100

    PupilDataVisualizer

--- Assets/_Scripts/PupilVisualizer.cs
+++ Assets/_Scripts/PupilVisualizer.cs
@@ -11,9 +11,9 @@ public class PupilVisualizer
     private Vector2 norm_pos_right;
     private Vector3 pupil_center_right;
     private float diameter_left;
-    private Vector3 rotation_left;
+    private float rotation_left;
     private float diameter_right;
-    private Vector3 rotation_right;
+    private float rotation_right;
     private GameObject left_pupil;
     private GameObject right_pupil;
 
@@ -31,25 +31,24 @@ public class PupilVisualizer
         left_container = GameObject.Find("LeftEyePupilContainer");
         right_container = GameObject.Find("RightEyePupilContainer");
 
-        SetPupil(left_pupil, norm_pos_left, diameter_left, rotation_left, left_container.transform);
-        SetPupil(right_pupil, norm_pos_right, diameter_right, rotation_right, right_container.transform);
+        SetPupil(left_pupil, norm_pos_left, diameter_left, left_container.transform);
+        SetPupil(right_pupil, norm_pos_right, diameter_right, right_container.transform);
     }
 
-    private void SetPupil(GameObject pupil, Vector3 norm_pos, float diameter, Vector3 rotation, Transform container)
+    private void SetPupil(GameObject pupil, Vector3 norm_pos, float diameter, Transform container)
     {
         pupil.GetComponent<Renderer>().material.color = Color.red;
         pupil.transform.parent = container;
         pupil.transform.localPosition = new Vector3(norm_pos.x * 10,0,norm_pos.y * 10);
-        pupil.transform.localRotation = Quaternion.Euler(rotation.x, rotation.y, rotation.z);
-        //pupil.transform.localScale = new Vector3(diameter/container.transform.parent.GetComponent<Renderer>().bounds.max.x, 0.1f, diameter/container.transform.parent.GetComponent<Renderer>().bounds.max.z);
-        pupil.transform.localScale = new Vector3(2.0f, 0.01f, 2.0f);
+        pupil.transform.localRotation = Quaternion.Euler(0,0,0);
+        pupil.transform.localScale = new Vector3(1.0f, 0.01f, 1.0f);
     }
 
     public void UpdatePupilsData()
     {
         UpdateData();
-        UpdatePupilData(left_pupil, norm_pos_left, rotation_left, diameter_left);
-        UpdatePupilData(right_pupil, norm_pos_right, rotation_right, diameter_right);
+        UpdatePupilData(left_pupil, norm_pos_left, diameter_left);
+        UpdatePupilData(right_pupil, norm_pos_right, diameter_right);
     }
 
     public void UpdateData()
@@ -57,20 +56,19 @@ public class PupilVisualizer
         norm_pos_left = pupilDataGetter.norm_pos_left;
         pupil_center_left = pupilDataGetter.pupil_center_left;
         diameter_left = pupilDataGetter.diameter_left;
-        rotation_left = pupilDataGetter.rotation_left;
+        rotation_left = pupilDataGetter.pupil_angle_left;
 
         norm_pos_right = pupilDataGetter.norm_pos_right;
         pupil_center_right = pupilDataGetter.pupil_center_right;
         diameter_right = pupilDataGetter.diameter_right;
-        rotation_right = pupilDataGetter.rotation_right;
+        rotation_right = pupilDataGetter.pupil_angle_right;
     }
 
-    private void UpdatePupilData(GameObject pupil, Vector3 norm_pos, Vector3 rotation, float diameter)
+    private void UpdatePupilData(GameObject pupil, Vector3 norm_pos, float diameter)
     {
         pupil.transform.localPosition = new Vector3(norm_pos.x * 10,0,norm_pos.y * 10);
-        pupil.transform.localRotation = Quaternion.Euler(rotation.x, rotation.y, rotation.z);
-        //pupil.transform.localScale = new Vector3(diameter, 0.1f, diameter);
-        pupil.transform.localScale = new Vector3(2.0f, 0.01f, 2.0f);
+        pupil.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        pupil.transform.localScale = new Vector3(1.0f, 0.01f, 1.0f);
     }
 
     public void DisablePupilData()
